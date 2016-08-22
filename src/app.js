0c63e3745244deddfb5f1bdf174ffa224af5331b
Matthijs Alles <admin@bixie.nl>
2016-08-22 02:32:31 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+
+import $ from 'jquery';
+
+$(() => {
+    console.log(`Hello!`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var assets = __dirname + "/web/assets";
+
+module.exports = [
+
+    {
+        entry: {
+            "app": "./src/app"
+        },
+        output: {
+            filename: "./web/bundle/[name].js"
+        },
+        resolve: {
+            alias: {
+                "jquery$": assets + "/jquery/dist/jquery.js",
+                "vue-form$": assets + "/vue-form/dist/vue-form.common.js",
+                "vue-resource$": assets + "/vue-resource/dist/vue-resource.common.js",
+                "JSONStorage$": assets + "/JSONStorage/storage.js"
+            }
+        },
+        module: {
+            loaders: [
+                {
+                    test: /(src|tests)(\/|\\).*\.js$/,
+                    loader: 'babel',
+                    query: { presets: ['es2015'] }
+                },
+                { test: /\.vue$/, loader: "vue" },
+                { test: /\.json$/, loader: "json" },
+                { test: /\.html$/, loader: "vue-html" }
+            ]
+        }
+    }
+
+];
